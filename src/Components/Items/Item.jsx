11533da8805fe81be5eceb4833c46d2cd451c443
iamMwirigi <mwirigimburugu81@gmail.com>
2024-06-06 11:49:47 +0300

    hero

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -5,7 +5,11 @@ import './Item.css'
 const Item = () => {
   return (
     <div className='item'>
-      
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        
+      </div>
     </div>
   )
 }
